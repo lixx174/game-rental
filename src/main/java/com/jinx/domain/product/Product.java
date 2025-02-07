6fd3fc39127c780033e3df9e43c952391f112340
lixinxuan <lixinxuan@zirugroup.com>
2025-02-07 15:58:30 +0800

    draft: product

---
+++ src/main/java/com/jinx/domain/product/Product.java
@@ -0,0 +1,19 @@
+package com.jinx.domain.product;
+
+import java.math.BigDecimal;
+
+/**
+ * 商品
+ *
+ * @author Jinx
+ */
+public class Product {
+
+    private String id;
+
+    private String name;
+
+    private String desc;
+
+    private BigDecimal price;
+}
